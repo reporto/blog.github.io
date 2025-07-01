d0536db891dad9ce7a477bd66ce10c63193e302b
luweijun <luweij@gmail.com>
2025-07-01 19:31:04 +0800

    blog theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    	output: 'static', // Required for GitHub Pages
-	site: 'https://reporto/blog.github.io',
-	integrations: [mdx(), sitemap()],
-    vite: {
-        plugins: [tailwindcss()]
-    }
+  output: "static", // Required for GitHub Pages
+  site: "https://reporto/blog.github.io",
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
